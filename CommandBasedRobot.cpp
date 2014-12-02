0e871eda37542e649f8fe1c08d9afc7809efe8ec
henrycrain <henryrcrain@gmail.com>
2014-12-02 15:43:33 -0500

    Added credits
    Feel free to adjust your titles.

--- CommandBasedRobot.cpp
+++ CommandBasedRobot.cpp
@@ -3,6 +3,18 @@
 #include "Commands/ExampleCommand.h"
 #include "CommandBase.h"
 
+/* 
+ *	Team 1740
+ *	2015 Code
+ *	Programming Staff:
+ *	Lord Kevin "Hjax" Konrad: High Overlord of Programming and Python / C++ grand master
+ *	Sire Henry Crain: Majordomo of Programming and Python / C++ master
+ *	Lady Allison "armadillokake" Konrad: Queen of the Armadillos and C++ n00b
+ *	Sir Jonathan Lei: Python / C++ dude
+ *	Our Lord, Charles Estabrooks: Programming God and C / C++ savior
+ *	Brian Healy the Eternal Champion: Programming God and Labview / TI-89 grand master
+ */
+
 class CommandBasedRobot : public IterativeRobot {
 private:
 	Command *autonomousCommand;
